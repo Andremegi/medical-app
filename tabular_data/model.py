4ffbacb7e90e0141aed83390f1fe085aa4c3e675
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-10-28 21:35:55 +0100

    finished mvp code

--- tabular_data/model.py
+++ tabular_data/model.py
@@ -31,33 +31,36 @@ def preprocessing_model_tabular_data(df):
 
     preprocessed_data_X= cancer_column_transformer.fit_transform(X)
 
-    return preprocessed_data_X, y_encoded
+    return pd.DataFrame(preprocessed_data_X), y_encoded
 
 #Encode the target and model with the preprocess data
 def modeling(df):
 
-    model_path = os.path.join(ROOT_PATH, 'models', 'mvp_model.pkl')
-    model = LogisticRegression.fit(preprocessing_model_tabular_data(df)[0], preprocessing_model_tabular_data(df)[1])
+    model_path = os.path.join(ROOT_PATH, 'tabular_data', 'models', 'mvp_model.pkl')
+    model = LogisticRegression()
+    model.fit(preprocessing_model_tabular_data(df)[0], preprocessing_model_tabular_data(df)[1])
 
     # Export the model as a pickle file, we might need to create a models folder on the taabolar data folder
     with open(model_path, 'wb') as file:
         pickle.dump(model, file)
 
-def prediction_tabular_data(gender,age, smoking,yellow_fingers, anxiety, chronic_disease, fatigue, allergy, wheezing, alcoholl, coughing, shortness_breath,swalloging_difficulty, chest_pain):
+def prediction_tabular_data(gender,age, smoking,yellow_fingers, anxiety, peer_pressure, chronic_disease, fatigue, allergy, wheezing, alcoholl, coughing, shortness_breath,swalloging_difficulty, chest_pain):
 
-    tabular_to_predict = pd.DataFrame((gender,smoking,yellow_fingers, anxiety, chronic_disease, fatigue, allergy, wheezing, alcoholl, coughing, shortness_breath,swalloging_difficulty, chest_pain, age), columns=['GENDER', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY',
+    tabular_to_predict = pd.DataFrame([[gender,smoking,yellow_fingers, anxiety, peer_pressure, chronic_disease, fatigue, allergy, wheezing, alcoholl, coughing, shortness_breath,swalloging_difficulty, chest_pain, age]], columns=['GENDER', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY',
        'PEER_PRESSURE', 'CHRONIC DISEASE', 'FATIGUE ', 'ALLERGY ', 'WHEEZING',
        'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH',
        'SWALLOWING DIFFICULTY', 'CHEST PAIN','AGE'])
 
-    model_path = os.path.join(ROOT_PATH, 'models', 'mvp_taboolar_data.pkl')
+    model_path = os.path.join(ROOT_PATH, 'tabular_data', 'models', 'mvp_model.pkl')
 
     #LOAD THE MODEL
     with open(model_path, 'rb') as file:
 
         model = pickle.load(file)
 
+    tabular_data_preprocessed = preprocessing_model_tabular_data(tabular_to_predict)
+
     #PREDICT
-    prediction = model.predict(tabular_to_predict)
+    prediction = model.predict(tabular_data_preprocessed[0])
 
     return prediction

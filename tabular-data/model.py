0f5cc76e720cc24ab89ab9fba708fa216017d73e
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-10-28 19:08:43 +0100

    seperated functions

--- tabular-data/model.py
+++ tabular-data/model.py
@@ -2,18 +2,61 @@ import os
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, LabelEncoder
 from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+import pickle
 
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
-def model_mvp():
-    model_path = os.path.join(ROOT_PATH,'../data/survey_lung_cancer.csv')
-    lung_cancer_df = pd.read_csv(model_path)
-    # seperating X and y (lung cancer target value) in case both are present
-    if 'LUNG_CANCER' in lung_cancer_df.columns:
-        #Create X,y
-        X = lung_cancer_df.iloc[:,lung_cancer_df.columns!='LUNG_CANCER']
-        y = lung_cancer_df.iloc[:,lung_cancer_df.columns=='LUNG_CANCER']
-    else:
-        X = lung_cancer_df
+#Create a function to preprocess X
+def preprocessing_model_tabular_data(df):
 
-    pipeline = Pipeline(OneHotEncoder())
+    if 'LUNG_CANCER' in df.columns:
+        X = df.iloc[:,df.columns!='LUNG_CANCER']
+        y = df.iloc[:,df.columns=='LUNG_CANCER']
+        label_encoder = LabelEncoder() # we create the labelEncoder from Sklearn
+        y_encoded = label_encoder.fit_transform(y) # We train and transform at the same time to obtain y_encoded
+        y_encoded = pd.DataFrame(y_encoded, columns=['LUNG_CANCER'])
+    else:
+        X = df
+        y_encoded = None
+
+    cancer_column_transformer= ColumnTransformer(
+    [('binary_data', OneHotEncoder(drop='if_binary'),['GENDER', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY',
+       'PEER_PRESSURE', 'CHRONIC DISEASE', 'FATIGUE ', 'ALLERGY ', 'WHEEZING',
+       'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH',
+       'SWALLOWING DIFFICULTY', 'CHEST PAIN']),
+    ('continues_data', MinMaxScaler(), ['AGE'])]
+)
+
+    preprocessed_data_X= cancer_column_transformer.fit_transform(X)
+
+    return preprocessed_data_X, y_encoded
+
+#Encode the target and model with the preprocess data
+def modeling(X):
+
+    model = LogisticRegression.fit(preprocessing_model_tabular_data(X)[0], preprocessing_model_tabular_data(X)[1])
+
+    # Export the model as a pickle file, we might need to create a models folder on the taabolar data folder
+    with open('../models/mvp_tabular_data.pkl', 'wb') as file:
+        pickle.dump(model, file)
+
+def prediction_tabular_data(gender,age, smoking,yellow_fingers, anxiety, chronic_disease, fatigue, allergy, wheezing, alcoholl, coughing, shortness_breath,swalloging_difficulty, chest_pain):
+
+    tabular_to_predict = pd.DataFrame((gender,smoking,yellow_fingers, anxiety, chronic_disease, fatigue, allergy, wheezing, alcoholl, coughing, shortness_breath,swalloging_difficulty, chest_pain, age), columns=['GENDER', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY',
+       'PEER_PRESSURE', 'CHRONIC DISEASE', 'FATIGUE ', 'ALLERGY ', 'WHEEZING',
+       'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH',
+       'SWALLOWING DIFFICULTY', 'CHEST PAIN','AGE'])
+
+    model_path = os.path.join(ROOT_PATH, 'models', 'mvp_taboolar_data.pkl')
+
+    #LOAD THE MODEL
+    with open(model_path, 'rb') as file:
+
+        model = pickle.load(file)
+
+    #PREDICT
+    prediction = model.predict(tabular_to_predict)
+
+    return prediction

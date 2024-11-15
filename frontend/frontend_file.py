f67a654f3fb6d8294a39473133f8bef11dc81d0d
Andrea Megias <andreamegias125@gmail.com>
2024-11-15 21:30:13 +0100

    Added Chatbot(RNN version simple)

--- frontend/frontend_file.py
+++ frontend/frontend_file.py
@@ -39,3 +39,26 @@ if uploaded_file is not None:
             st.write(f"{label}: {probability * 100:.2f}%")
     else:
         st.write("Failed to upload the image to the API.")
+
+
+
+st.title("Medical Chatbot")
+st.write("Please, describe your symtoms so I can predict your disease")
+
+
+txt = st.text_area('Write your symptoms here', '''
+
+    ''')
+params = {'symptoms': txt}
+url_text = 'https://textdata-gjldcvjueq-ew.a.run.app/symptoms'
+
+if st.button('Predict'):
+
+    # print is visible in the server output, not in the page
+    print(type(txt))
+    st.write(txt)
+    st.write('Predicting your desease...')
+    response_text = requests.get(url_text, params).json()
+    st.write(f"Your deseais is {response_text['disease']} with a probability = {round(float(response_text['probability']), 2)} %.")
+else:
+    st.write('Please press the button so I can make a prediction.')

896cd54eb3c5f45d9521c1a61acbcbf10d5f0e00
Marc-Alexandre Espiaut <marc-alexandre.espiaut@posteo.eu>
2024-11-19 20:51:22 +0100

    Renaming files and integrating the LLM chatbot into the new interface.

--- frontend/modules/nav.py
+++ frontend/modules/nav.py
@@ -4,6 +4,7 @@ import streamlit as st
 def Navbar():
     with st.sidebar:
         st.page_link('streamlit_app.py', label='Homepage', icon='🫁')
-        st.page_link('pages/images_analysis.py', label='Image Analysis', icon='🩻')
-        st.page_link('pages/chatbot.py', label='Chatbot', icon='👨🏻‍⚕️')
+        st.page_link('pages/images_analysis.py', label='X-Ray Analysis', icon='🩻')
+        st.page_link('pages/deep_learning_diagnostics.py', label='Deep Diagnostics', icon='🩺')
+        st.page_link('pages/llm_doctor_chatbot.py', label='LLM Doctor', icon='👨🏻‍⚕️')
 
---
+++ frontend/pages/llm_doctor_chatbot.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import requests
+
+from modules.nav import Navbar
+
+
+def main():
+    Navbar()
+
+    st.title("Hugging face Chatbot [beta]")
+    st.write("Please, describe your symtoms so I can predict your disease")
+
+    chat_history = ""
+
+    # Function to call the API (assuming you are running it locally on a FastAPI server)
+    def call_api(user_input, chat_history):
+        response = requests.get(
+            "http://localhost:8000/chat-bot",
+            params={"user_input": user_input, "chat_history": chat_history},
+        )
+        return response.json()
+
+    user_input = st.text_area(
+        "Write your symptoms here",
+        """
+    
+        """,
+    )
+    # When the user clicks the 'Ask' button
+    if st.button("Ask"):
+        if user_input:
+            # Call the API with the current user input and the previous chat history
+            result = call_api(user_input, chat_history)
+
+            # Display the user input and the bot's response
+            chat_history += result["chat_history"]
+
+            # Show the updated chat history
+            st.text_area("Chat History", value=chat_history, height=300)
+
+    # Display the current chat history (user's messages and bot's responses)
+    st.text_area("Chat History", value=chat_history, height=300, disabled=True)
+
+
+if __name__ == "__main__":
+    main()

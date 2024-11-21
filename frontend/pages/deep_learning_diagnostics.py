e47c4d3176b7d1021753aaffc856e0dd8d1a2e81
Andrea Megias <andreamegias125@gmail.com>
2024-11-21 11:32:48 +0100

    Changed frontend

--- frontend/pages/deep_learning_diagnostics.py
+++ frontend/pages/deep_learning_diagnostics.py
@@ -20,17 +20,51 @@ def main():
 
     if st.button("Predict"):
 
-        # print is visible in the server output, not in the page
-        print(type(txt))
-        st.write(txt)
-        st.write("Predicting your desease...")
         response_text = requests.get(url_text, params).json()
         st.write(
             f"Your deseais is {response_text['disease']} with a probability = {round(float(response_text['probability']), 2)} %."
         )
+        st.markdown(
+        "<h3 style='margin:1; padding:1'>Your desease is:</h3>",
+        unsafe_allow_html=True,
+            )
+
+
+        st.metric(' ',response_text['disease'], ' ')
     else:
         st.write("Please press the button so I can make a prediction.")
 
+
+    st.markdown(
+        """
+    <style>
+    .stMetric {
+        font-weight: bold;
+        text-align: center;
+        background-color: #D3D3D3;
+        margin: auto;
+
+        width : auto;
+
+    }
+    .stButton > button {
+        background-color: #4daab2;
+        color: white;
+    }
+    .stButton > button:hover {
+        background-color: #FFFFFF; /* WHITE */
+        color: #4daab2;
+        border-style: solid;
+        border-radius: 8px;
+        border-color: #4daab2;
+
+
+    }
+    }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
 
 if __name__ == "__main__":
     main()
